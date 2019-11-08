71e8ed9169c0ca7d4efb5ce121a57591a16e9f26
yc20170111 <57425001+yc20170111@users.noreply.github.com>
2019-11-09 07:35:32 +0800

    Add treeArray.

---
+++ treeArray.h
@@ -0,0 +1,44 @@
+/*treeArray*/
+#ifndef CSTDIO_INCLUDED
+#include<cstdio>
+#define SCTDIO_INCLUDED
+#endif
+
+#ifndef VECTOR_INCLUDED
+#include<vector>
+#define VECTOR_INCLUDED
+#endif
+
+template <typename TId, typename TValue> class treeArray {
+	private:
+		TId nodeSize;
+		std::vector<TValue>tree;
+	public:
+		TId lowbit(TId x) {
+			return x&-x;
+		}
+		void add(TId u, TValue k) {
+			while(u<=nodeSize) {
+				tree[u]+=k;
+				u+=lowbit(u);
+			}
+		}
+		void add(TId u, TId v, TValue k) {
+			add(u, k);
+			if(v<nodeSize) {
+				add(v+1, -k);
+			}
+		}
+		TValue query(TId u) {
+			TValue sum=0;
+			while(u>0) {
+				sum+=tree[u];
+				u-=lowbit(u);
+			}
+			return sum;
+		}
+		treeArray(TId size) {
+			nodeSize=size;
+			tree.resize(nodeSize+1);
+		}
+};
