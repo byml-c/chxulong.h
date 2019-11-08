76abc8f37d3520aff74f2ccb5f778167e393f9d6
Young Newfield <43328428+jiyu596@users.noreply.github.com>
2019-11-08 18:30:53 +0800

    Update graph.h
    emm. change it into typename and deleted printing.

--- graph.h
+++ graph.h
@@ -13,33 +13,34 @@
 	#define ASSERT_H_INCLUDED
 #endif
 
-class Graph {
+template <typename Tp> class Graph {
 	private:
-		int nodeSize;
-		std::vector<std::vector<std::pair<int,int> > >edges;
+		Tp nodeSize;
+		std::vector<std::vector<std::pair<Tp,Tp> > >edges;
 	public:
-		void insert(int u,int v,int val) {
+		void insert(Tp u,Tp v,Tp val) {
 			assert(u<nodeSize&&v<nodeSize);
 			edges[u].push_back(std::make_pair(v,val));
 		}
-		void printOutDegree(int u,const char* format) {
-			for(std::pair<int,int> i:edges[u]) {
-				printf(format,i.first,i.second);
-			}
-		}
-		void printOutDegree(int u) {
-			printOutDegree(u,"[%d,%d]");
-		}
-		void printOutDegree() {
-			for(int i=0;i<nodeSize;i++) {
-				printOutDegree(i);
-				puts("");
-			}
-		}
-		int getNodeSize() {
+		//no need.
+//		void printOutDegree(Tp u,const char* format) {
+//			for(std::pair<Tp,Tp> i:edges[u]) {//auto i?
+//				prTpf(format,i.first,i.second);
+//			}
+//		}
+//		void printOutDegree(Tp u) {
+//			prTpOutDegree(u,"[%d,%d]");
+//		}
+//		void printOutDegree() {
+//			for(Tp i=0;i<nodeSize;i++) {
+//				prTpOutDegree(i);
+//				puts("");
+//			}
+//		}
+		Tp getNodeSize() {
 			return nodeSize;
 		}
-		Graph(int size) {
+		Graph(Tp size) {
 			edges.resize(size);
 			nodeSize=size;
 		}
