9739b66b497168e8a8c02a8d5409089d3acd2f11
yc20170111 <57425001+yc20170111@users.noreply.github.com>
2020-01-13 20:42:58 +0800

    Add files via upload
    Add Segment Tree for RMQ and SUM. And fix some bugs. But I have added much more bugs.

--- SegmentTree.h
+++ SegmentTree.h
@@ -1,3 +1,4 @@
+/*Segment Tree*/
 #ifndef CSTDIO_INCLUDED
 #include<cstdio>
 #define CSTDIO_INCLUDED
@@ -105,26 +106,150 @@ template <typename TId, typename TValue> class RMQSegmentTree {
 			INF=EdgeValue;
 			Revise(1,Size,BeginValue);
 		}
-		void Add(TId Pos,TValue Value){
+		void Add(TId Pos,TValue Value)
+		{
 			RangeAdd(1,NodeSize,1,Pos,Pos,Value);
 		}
-		void Revise(TId Pos,TValue Value){
+		void Revise(TId Pos,TValue Value)
+		{
 			RangeRevise(1,NodeSize,1,Pos,Pos,Value);
 		}
-		TValue Query(TId Pos){
+		TValue Query(TId Pos)
+		{
 			return RangeQuery(1,NodeSize,1,Pos,Pos);
 		}
-		void Add(TId Pos_left,TId Pos_right,TValue Value){
+		void Add(TId Pos_left,TId Pos_right,TValue Value)
+		{
 			RangeAdd(1,NodeSize,1,Pos_left,Pos_right,Value);
 		}
-		void Revise(TId Pos_left,TId Pos_right,TValue Value){
+		void Revise(TId Pos_left,TId Pos_right,TValue Value)
+		{
 			RangeRevise(1,NodeSize,1,Pos_left,Pos_right,Value);
 		}
-		TValue Query(TId Pos_left,TId Pos_right){
+		TValue Query(TId Pos_left,TId Pos_right)
+		{
 			return RangeQuery(1,NodeSize,1,Pos_left,Pos_right);
 		}
 		void Print(){
 			int i=1,j=1;
+			while(j<NodeSize)++i,j<<=1;
+			PrintTree(1,i,Tree);
+			PrintTree(1,i,Lazy);
+		}
+};
+
+template <typename TId, typename TValue> class SUMSegmentTree{
+	private:
+		std::vector<TValue>Tree,Lazy;
+		TId NodeSize;
+		
+		void Swap(TId& a,TId& b){TId t=a;a=b;b=t;}
+		void RangeRevise(TId lf,TId rt,TId tot,TId ll,TId rr,TValue k){
+			if(rt<ll||lf>rr){
+				return ;
+			}else if(lf>=ll&&rt<=rr){
+				if(lf==rt){
+					Tree[tot]=k;Lazy[tot]=0;
+				}else{
+					TId mid=(lf+rt)>>1;
+					RangeRevise(lf,mid,tot<<1,ll,rr,k);
+					RangeRevise(mid+1,rt,tot<<1|1,ll,rr,k);
+					Lazy[tot]=0;
+					Tree[tot]=Tree[tot<<1]+Tree[tot<<1|1];
+				}
+			}else{
+				TId mid=(lf+rt)>>1;
+				RangeRevise(lf,mid,tot<<1,ll,rr,k);
+				RangeRevise(mid+1,rt,tot<<1|1,ll,rr,k);
+				Tree[tot]=Tree[tot<<1]+Tree[tot<<1|1];
+			}
+		}
+		void SpreadTag(TId lf,TId rt,TId tot){
+			if(Lazy[tot]){
+				TId mid=(lf+rt)>>1;
+				Lazy[tot<<1]+=Lazy[tot];
+				Lazy[tot<<1|1]+=Lazy[tot];
+				Tree[tot<<1]+=Lazy[tot]*(mid-lf+1);
+				Tree[tot<<1|1]+=Lazy[tot]*(rt-mid);
+				Lazy[tot]=0;
+			}
+		}
+		void RangeAdd(TId lf,TId rt,TId tot,TId ll,TId rr,TValue k){
+			if(rt<ll||lf>rr){
+				return ;
+			}else if(lf>=ll&&rt<=rr){
+				Tree[tot]+=k*(rt-lf+1);
+				Lazy[tot]+=k;
+			}else{
+				SpreadTag(lf,rt,tot);
+				TId mid=(lf+rt)>>1;
+				RangeAdd(lf,mid,tot<<1,ll,rr,k);
+				RangeAdd(mid+1,rt,tot<<1|1,ll,rr,k);
+				Tree[tot]=Tree[tot<<1]+Tree[tot<<1|1];
+			}
+		}
+		TValue RangeQuery(TId lf,TId rt,TId tot,TId ll,TId rr){
+			if(rt<ll||lf>rr){
+				return 0;
+			}else if(lf>=ll&&rt<=rr){
+				return Tree[tot];
+			}else{
+				SpreadTag(lf,rt,tot);
+				TId mid=(lf+rt)>>1;
+				TValue v1=RangeQuery(lf,mid,tot<<1,ll,rr);
+				TValue v2=RangeQuery(mid+1,rt,tot<<1|1,ll,rr);
+				return v1+v2;
+			}
+		}
+		void PrintTree(TId d,TId td,std::vector<TValue>& Tree){
+			if(d>td)return ;
+			for(TId i=1;i<(1<<(td-d));++i)
+				printf(" ");
+			TId lf=1<<(d-1),rt=(1<<d)-1;
+			for(TId i=lf;i<=rt;++i){
+				printf("%d",Tree[i]);
+				if(i==rt)continue;
+				for(TId j=1;j<(1<<(td-d+1));++j)
+					printf(" ");
+			}printf("\n");
+			PrintTree(d+1,td,Tree);
+		}
+	public:
+		SUMSegmentTree(TId Size,TValue BeginValue){
+			NodeSize=Size;
+			Tree.resize(Size*4);Tree.clear();
+			Lazy.resize(Size*4);Tree.clear();
+			revise(1,Size,BeginValue);
+		}
+		void revise(TId Pos,TValue Value)
+		{
+			RangeRevise(1,NodeSize,1,Pos,Pos,Value);
+		}
+		void revise(TId Pos_left,TId Pos_right,TValue Value)
+		{
+			if(Pos_left>Pos_right)Swap(Pos_left,Pos_right);
+			RangeRevise(1,NodeSize,1,Pos_left,Pos_right,Value);
+		}
+		void add(TId Pos,TValue Value)
+		{
+			RangeAdd(1,NodeSize,1,Pos,Pos,Value);
+		}
+		void add(TId Pos_left,TId Pos_right,TValue Value)
+		{
+			if(Pos_left>Pos_right)Swap(Pos_left,Pos_right);
+			RangeAdd(1,NodeSize,1,Pos_left,Pos_right,Value);
+		}
+		TValue query(TId Pos)
+		{
+			return RangeQuery(1,NodeSize,1,Pos,Pos);
+		}
+		TValue query(TId Pos_left,TId Pos_right)
+		{
+			if(Pos_left>Pos_right)Swap(Pos_left,Pos_right);
+			return RangeQuery(1,NodeSize,1,Pos_left,Pos_right);
+		}
+		void Print(){
+			TId i=1,j=1;
 			while(j<NodeSize)++i,j<<=1;
 			PrintTree(1,i,Tree);
 			PrintTree(1,i,Lazy);
